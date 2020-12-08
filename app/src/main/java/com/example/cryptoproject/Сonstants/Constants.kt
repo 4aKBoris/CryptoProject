109dd9f833405f923df4c5eaee7eca511766f47d
4aKBoris <lda-1028@mail.ru>
2020-12-08 19:36:00 +0300

    Правки

---
+++ app/src/main/java/com/example/cryptoproject/Сonstants/Constants.kt
@@ -0,0 +1,13 @@
+@file:Suppress("PackageName")
+
+package com.example.cryptoproject.Сonstants
+
+import android.annotation.SuppressLint
+
+const val NotUse = "Не использовать"
+@SuppressLint("SdCardPath")
+const val PATH_KEY_STORE = "/data/data/com.example.cryptoproject/my_keystore.ks"
+const val KEY_STORE_ALGORITHM = "PKCS12"
+const val ALGORITHM = "SHA256withRSA"
+const val RSA = "RSA"
+const val BlockSize = 128
